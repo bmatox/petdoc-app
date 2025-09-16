0e1fdf37cadd7bff2e47299fd977813124e882bb
51644 <51644@tjce.jus.br>
2025-09-16 16:32:54 -0300

    feat: Inicia o setup de Spring Boot para o Backend

---
+++ backend/petdoc-app/src/main/java/com/petdoc/backend/PetdocAppApplication.java
@@ -0,0 +1,13 @@
+package com.petdoc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdocAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetdocAppApplication.class, args);
+    }
+
+}
