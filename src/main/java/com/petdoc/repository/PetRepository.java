3f1428f347a132fc421ed5e884afe716410a2ba0
Joao Pedro Rego <joaomagdev@gmail.com>
2025-09-29 20:44:26 -0300

    feat(vacina): Adiciona estrutura mvc para cadastro de vacinas

---
+++ src/main/java/com/petdoc/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package com.petdoc.repository;
+import com.petdoc.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {}
